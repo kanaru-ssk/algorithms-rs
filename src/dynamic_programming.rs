3bf1fec87bcca651b0d956f762df26c8d1e6b211
kanaru-ssk <2019january.0131@gmail.com>
2023-12-03 16:53:21 +0900

    Abstract dynamic_programming

--- src/dynamic_programming.rs
+++ src/dynamic_programming.rs
@@ -1,45 +1,23 @@
-use super::gcd::gcd_vec;
-use velcro::vec;
+pub fn dynamic_programming<T, U>(
+    capacity: usize,
+    items: &Vec<U>,
+    calc_cell: fn(items: &Vec<U>, table: &Vec<Vec<T>>, y: usize, x: usize) -> T,
+) -> T {
+    let table_h = items.len() + 1;
+    let table_w = capacity + 1;
+    let mut table: Vec<Vec<T>> = Vec::new();
 
-pub struct Item {
-    id: u32,
-    cost: u32,
-    value: u32,
-}
-
-pub fn dynamic_programming(capacity: u32, items: &Vec<Item>) -> u32 {
-    // 最大公約数で割ってtableの大きさを最適化
-    let costs: Vec<u32> = items.iter().map(|item| item.cost).collect();
-    let gcd: u32 = gcd_vec(&vec![capacity, ..costs]);
-    let capacity = capacity / gcd;
-    let items: Vec<Item> = items
-        .iter()
-        .map(|item| Item {
-            id: item.id,
-            cost: item.cost / gcd,
-            value: item.value,
-        })
-        .collect();
-
-    let num_item = items.len();
-    let mut table: Vec<Vec<u32>> = vec![vec![0; capacity as usize + 1]; num_item];
-
-    for i in 1..num_item {
-        for j in 0..=capacity as usize {
-            if items[i].cost > j as u32 {
-                // items[i]が入らない場合 : 上のマスを代入
-                table[i][j] = table[i - 1][j];
-            } else {
-                // items[i]が入る場合 : 上のマスと比較して大きい方を代入
-                table[i][j] = std::cmp::max(
-                    table[i - 1][j - items[i].cost as usize] + items[i].value,
-                    table[i - 1][j],
-                )
+    for y in 0..table_h {
+        for x in 0..table_w as usize {
+            if x == 0 {
+                table.push(vec![]);
             }
+            let cell = calc_cell(items, &table, y, x);
+            table[y].push(cell);
         }
     }
 
-    table[num_item - 1][capacity as usize]
+    table.pop().unwrap().pop().unwrap()
 }
 
 #[cfg(test)]
@@ -47,82 +25,64 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_bin_packing() {
-        let items = self::vec![
-            Item {
-                id: 1,
-                cost: 150,
-                value: 150
-            },
-            Item {
-                id: 2,
-                cost: 200,
-                value: 200
-            },
-            Item {
-                id: 3,
-                cost: 250,
-                value: 250
-            },
-            Item {
-                id: 4,
-                cost: 200,
-                value: 200
-            },
-            Item {
-                id: 5,
-                cost: 50,
-                value: 50
+    fn test_fibonacci() {
+        fn calc_cell(_: &Vec<u32>, table: &Vec<Vec<u32>>, y: usize, x: usize) -> u32 {
+            if x <= 0 {
+                0
+            } else if x <= 2 {
+                1
+            } else {
+                table[y][x - 1] + table[y][x - 2]
             }
-        ];
+        }
+        assert_eq!(dynamic_programming(6, &Vec::new(), calc_cell), 8);
+    }
 
-        let capacity = 500;
-        let result = dynamic_programming(capacity, &items);
-        assert_eq!(result, 500);
-        assert_eq!(result == capacity, true);
-
-        let capacity = 720;
-        let result = dynamic_programming(capacity, &items);
-        assert_eq!(result, 700);
-        assert_eq!(result == capacity, false);
+    #[test]
+    fn test_bin_packing() {
+        let capacity = 10;
+        let items: Vec<usize> = vec![4, 7, 8, 5, 1];
+        fn calc_cell(items: &Vec<usize>, table: &Vec<Vec<bool>>, y: usize, x: usize) -> bool {
+            if y == 0 {
+                x == 0
+            } else if table[y - 1][x] {
+                true
+            } else if x < items[y - 1] {
+                false
+            } else {
+                table[y - 1][x - items[y - 1]]
+            }
+        }
+        assert_eq!(dynamic_programming(capacity, &items, calc_cell), true);
     }
 
     #[test]
     fn test_knapsack() {
-        let items = self::vec![
-            Item {
-                id: 1,
-                cost: 150,
-                value: 4
-            },
-            Item {
-                id: 2,
-                cost: 200,
-                value: 7
-            },
-            Item {
-                id: 3,
-                cost: 250,
-                value: 8
-            },
-            Item {
-                id: 4,
-                cost: 200,
-                value: 5
-            },
-            Item {
-                id: 5,
-                cost: 50,
-                value: 1
-            }
+        struct Item {
+            cost: u32,
+            value: u32,
+        }
+        let capacity = 10;
+        let items = vec![
+            Item { cost: 3, value: 4 },
+            Item { cost: 4, value: 7 },
+            Item { cost: 5, value: 8 },
+            Item { cost: 4, value: 5 },
+            Item { cost: 1, value: 1 },
         ];
 
-        let capacity = 500;
-        let result = dynamic_programming(capacity, &items);
-        assert_eq!(result, 16);
-
-        let capacity = 720;
-        let result = dynamic_programming(capacity, &items);
-        assert_eq!(result, 21);
+        fn calc_cell(items: &Vec<Item>, table: &Vec<Vec<u32>>, y: usize, x: usize) -> u32 {
+            if y == 0 {
+                0
+            } else if items[y - 1].cost > x as u32 {
+                table[y - 1][x]
+            } else {
+                std::cmp::max(
+                    table[y - 1][x - items[y - 1].cost as usize] + items[y - 1].value,
+                    table[y - 1][x],
+                )
+            }
+        }
+        assert_eq!(dynamic_programming(capacity, &items, calc_cell), 16);
     }
 }
